504f6e342adab616231ce4b9ab7b40ed694369e4
Joeri <klomp.joeri@icloud.com>
2022-10-23 23:32:55 +0200

    added an about page with link to and from the main page

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import {Link} from "gatsby";
+
+const aboutPage = () => {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <p>Hi there! You shouldn't be interested in me, I'm not even Giel! Or Daan! You should meet them!</p>
+            <Link to={"/"}>Back</Link>
+        </main>
+    )
+}
+
+export const Head = () => <title>About Me</title>
+//Gatsby lets us define metadata like title with the Gatsby Head api. You just have to export a component called Head
+//from the page template to apply the metadata. It works because we're exporting Head in aan page inside src/pages.
+
+//pages created in src/pages directory use the name of the file as the route for the page
+
+export default aboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from "react";
+import {Link} from "gatsby";
 
 const indexPage = () => {
     return (
         <main>
             <h1>Welcome to my Giel worship page!</h1>
+            <Link to={"/about"}>About</Link>
             <p>This is where we worship the birth of Giel.</p>
         </main>
     )
